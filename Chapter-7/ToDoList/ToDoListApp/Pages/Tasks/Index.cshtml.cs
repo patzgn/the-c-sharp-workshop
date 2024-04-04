5d881aee4050b6ceb08d202aa18f4df85e5c5b41
patzgn <103074854+patzgn@users.noreply.github.com>
2024-04-04 18:24:43 +0200

    Refactor a tag helper to a partial page with custom logic

--- Chapter-7/ToDoList/ToDoListApp/Pages/Tasks/Index.cshtml.cs
+++ Chapter-7/ToDoList/ToDoListApp/Pages/Tasks/Index.cshtml.cs
@@ -1,23 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoListApp.Data;
 using ToDoListApp.Models;
 
 namespace ToDoListApp.Pages.Tasks;
 
 public class IndexModel : PageModel
 {
-    public IList<ToDoTask> Tasks { get; set; } = new List<ToDoTask>();
+	public IList<ToDoTask> Tasks { get; set; } = new List<ToDoTask>();
 
-    public IndexModel()
-    {
-    }
+	private readonly ToDoDbContext _context;
 
-    public void OnGet()
-    {
-        Tasks = new List<ToDoTask>
-        {
-            new ToDoTask("Create", ETaskStatus.ToDo),
-            new ToDoTask("Creating", ETaskStatus.Doing),
-            new ToDoTask("Created", ETaskStatus.Done),
-        };
-    }
+	public IndexModel(ToDoDbContext context)
+	{
+		_context = context;
+	}
+
+	public void OnGet()
+	{
+		Tasks = _context.Tasks.ToList();
+	}
+
+	public IActionResult OnPostStartTask(Guid id)
+	{
+		return ChangeTaskStatus(id, ETaskStatus.ToDo, ETaskStatus.Doing);
+	}
+
+	public IActionResult OnPostEndTask(Guid id)
+	{
+		return ChangeTaskStatus(id, ETaskStatus.Doing, ETaskStatus.Done);
+	}
+
+	private IActionResult ChangeTaskStatus(Guid id, ETaskStatus current, ETaskStatus next)
+	{
+		var task = _context.Find<ToDoTask>(id);
+
+		if (task != null && task.Status == current)
+		{
+			task.Status = next;
+
+			_context.Update(task);
+
+			_context.SaveChanges();
+		}
+
+		Tasks = _context.Tasks.ToList();
+
+		Request.QueryString = default;
+
+		return RedirectToPage("Index");
+	}
 }
