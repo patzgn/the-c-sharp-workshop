11eade4d58e5a2cf9c8c47c0dda14a4c7da55c7f
patzgn <103074854+patzgn@users.noreply.github.com>
2024-04-04 18:41:28 +0200

    Create a page to edit an existing task

---
+++ Chapter-7/ToDoList/ToDoListApp/Pages/Tasks/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoListApp.Data;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Pages.Tasks;
+
+public class EditModel : PageModel
+{
+	public ToDoTask Task { get; set; }
+
+	private readonly ToDoDbContext _context;
+
+	public EditModel(ToDoDbContext context)
+	{
+		_context = context;
+	}
+
+	public IActionResult OnGet(Guid id)
+	{
+		Task = _context.Tasks.Find(id);
+
+		if (Task is null)
+		{
+			return RedirectToPage("Create");
+		}
+
+		return Page();
+	}
+
+	public async Task<IActionResult> OnPostAsync(Guid id)
+	{
+		var task = _context.Tasks.Single(t => t.Id == id);
+
+		if (await TryUpdateModelAsync(task, "Task"))
+		{
+			_context.Update(task);
+
+			await _context.SaveChangesAsync();
+
+			TempData["SuccessMessage"] = "Task successfully updated";
+
+			return RedirectToPage("Index");
+		}
+
+		return Page();
+	}
+}
